8d8b31d344decb442f1de634b105c6da22455939
JonatasFAlves <alves.jnts@gmail.com>
2017-09-23 01:04:29 -0300

    Added listenEvent, readLocation and updateProfile functions

--- home.js
+++ home.js
@@ -1,23 +1,52 @@
-(function () {
-    var uid;
+function initApp() {
+
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            uid = user.uid;
+            let uid = user.uid;
+            let name = user.displayName;
+
+            alert(name)
+
+        } else {
+            // window.location('login.html');
+            alert('Usuário não logado');
         }
     });
 
-    var name = 'Jnts';
-    var email = 'alves.jnts@gmail.com';
-    var imageUrl = 'http://www.iconarchive.com/download/i86449/martin-berube/flat-animal/turtle.ico';
+};
 
-    function writeUserData(uid, name, email, imageUrl) {
-        firebase.database().ref('users/' + uid).set({
-            username: name,
-            email: email,
-            profile_picture: imageUrl
-        }).catch(e => console.log(e.message));
-    }
+function update() {
+    user.updateProfile({
+        displayName: "Jane Q. User",
+        photoURL: "https://example.com/jane-q-user/profile.jpg"
+    }).then(function () {
+        // Update successful.
+    }).catch(function (error) {
+        // An error happened.
+    });
+}
 
+function readLocation(uid) {
 
-    writeUserData(uid, name, email, imageUrl);
-}());
+    firebase.database().ref('users/' + uid).once('value').then(function (snapshot) {
+        alert(snapshot.val().location);
+    });
+}
+
+function listenEvent() {
+    var locationRef = firebase.database().ref('users/' + uid + '/location');
+    locationRef.on('value', function (snapshot) {
+        alert(snapshot.val());
+    });
+}
+
+function writeUserData(uid, name, location) {
+    firebase.database().ref('users/' + uid).set({
+        username: name,
+        location: location
+    }).catch(e => console.log(e.message));
+}
+
+window.onload = function () {
+    initApp();
+};
