41425e25b0594c3d0f96a978db40e5660d382027
JonatasFAlves <alves.jnts@gmail.com>
2017-09-26 01:53:50 -0300

    Improvisations in js

--- init.js
+++ init.js
@@ -1,12 +1,10 @@
-(function () {
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyA5zKK8PhcBAonQVfnRNspagRKKLb_8bFc",
-        authDomain: "find-bc.firebaseapp.com",
-        databaseURL: "https://find-bc.firebaseio.com",
-        projectId: "find-bc",
-        storageBucket: "",
-        messagingSenderId: "342346757855"
-    };
-    firebase.initializeApp(config);
-}());
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA5zKK8PhcBAonQVfnRNspagRKKLb_8bFc",
+    authDomain: "find-bc.firebaseapp.com",
+    databaseURL: "https://find-bc.firebaseio.com",
+    projectId: "find-bc",
+    storageBucket: "",
+    messagingSenderId: "342346757855"
+};
+firebase.initializeApp(config);
--- login.js
+++ login.js
@@ -1,32 +1,37 @@
-(function () {
-    const txtEmail = document.getElementById('txtEmail');
-    const txtPass = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
+const txtEmail = document.getElementById('txtEmail');
+const txtPass = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+
+function initApp() {
 
     btnLogin.addEventListener('click', function () {
+        var email = txtEmail.value;
+        var password = txtPass.value;
 
         if (firebase.auth().currentUser) {
             firebase.auth().signOut();
-        } else {
-
-            const email = txtEmail.value;
-            const pass = txtPass.value;
-            const auth = firebase.auth();
-
-            auth.signInWithEmailAndPassword(email, pass)
-                .then(alert('helloworld'))
-                .catch(e => e.message);
-                
-
         }
 
+        signIn(email, password);
 
     });
+    
+};
 
-    firebase.auth().onAuthStateChanged(function(user) {
+function signIn(email, pass) {
+    firebase.auth().signInWithEmailAndPassword(email, pass).then(alert()).catch(function (error) {
+        alert(error.message);
+    });
+}
+
+window.onload = function () {
+    initApp();
+
+    //If the code is working, then test it inside the initApp function.
+    firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-         window.location = 'home.html';
+            window.location.href = 'home.html';
         }
-      });
-
-}());
+    });
+    
+};
