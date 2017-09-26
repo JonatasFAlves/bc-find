74be12f665cbde7a475c12c6d2495eed1d980fe3
JonatasFAlves <alves.jnts@gmail.com>
2017-09-26 01:53:17 -0300

    Edited location change and location event listener functions

--- home.js
+++ home.js
@@ -1,19 +1,37 @@
+var userName;
+
 function initApp() {
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            let uid = user.uid;
-            let name = user.displayName;
-
-            alert(name)
-
+            userName = user.displayName;
         } else {
-            // window.location('login.html');
             alert('Usuário não logado');
+            window.location.href = 'login.html';
         }
     });
 
-};
+    var locationRef = firebase.database().ref('location/');
+    locationRef.on('child_changed', function(data) {
+        alert(data.key);
+        alert(data.val());
+    });
+    
+
+}
+
+function updateLocation(userName, location) {
+    // Informations entries.
+    var locationData = {
+        [userName]: location
+    };
+
+    // Write the user location data in location.
+    var updates = {};
+    updates['location/'] = locationData;
+
+    return firebase.database().ref().update(updates);
+}
 
 function update() {
     user.updateProfile({
@@ -24,27 +42,6 @@ function update() {
     }).catch(function (error) {
         // An error happened.
     });
-}
-
-function readLocation(uid) {
-
-    firebase.database().ref('users/' + uid).once('value').then(function (snapshot) {
-        alert(snapshot.val().location);
-    });
-}
-
-function listenEvent() {
-    var locationRef = firebase.database().ref('users/' + uid + '/location');
-    locationRef.on('value', function (snapshot) {
-        alert(snapshot.val());
-    });
-}
-
-function writeUserData(uid, name, location) {
-    firebase.database().ref('users/' + uid).set({
-        username: name,
-        location: location
-    }).catch(e => console.log(e.message));
 }
 
 window.onload = function () {
