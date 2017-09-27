4ca84ec3df2536fd46d8c88cebf989abe6b37acb
JonatasFAlves <alves.jnts@gmail.com>
2017-09-27 02:07:27 -0300

    Testing with login/write in this page

--- login.js
+++ login.js
@@ -2,7 +2,19 @@ const txtEmail = document.getElementById('txtEmail');
 const txtPass = document.getElementById('txtPassword');
 const btnLogin = document.getElementById('btnLogin');
 
-function initApp() {
+var userName;
+
+function initApp() {    
+    //If the code is working, then test it inside the initApp function.
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            userName = user.displayName;            
+            firebase.database().ref('user-locations/').set({
+                [userName]: 'Sala Suporte'
+            });
+            window.location.href = 'home.html';
+        }
+    });
 
     btnLogin.addEventListener('click', function () {
         var email = txtEmail.value;
@@ -13,25 +25,30 @@ function initApp() {
         }
 
         signIn(email, password);
-
     });
-    
+
 };
 
 function signIn(email, pass) {
-    firebase.auth().signInWithEmailAndPassword(email, pass).then(alert()).catch(function (error) {
-        alert(error.message);
-    });
+    return firebase.auth().signInWithEmailAndPassword(email, pass)
+        .then(alert('Logged In'))
+        .catch(function (err) {
+            if (err) {
+                alert(err.message);
+            }
+        });
 }
+
+// function writeUserLocation(userName, location) {
+//     return firebase.database().ref('user-locations/').set({ [userName]: location });
+// }
+
+function writeUserLocation(name, location) {
+    return firebase.database().ref('user-locations/').set({
+        [userName]: location
+    });
+  }
 
 window.onload = function () {
     initApp();
-
-    //If the code is working, then test it inside the initApp function.
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            window.location.href = 'home.html';
-        }
-    });
-    
 };
